f67afd5c168eb8c44bd25223cfd2e331230b395b
MasatoTakakusaki <mtkksk1780@gmail.com>
2024-06-29 23:47:25 -0700

    Implement admin's register function(Frontend)

--- frontend/src/app/helper/validationUtils.ts
+++ frontend/src/app/helper/validationUtils.ts
@@ -12,24 +12,24 @@ export const checkRegisterValidation = ({
   password,
   passConfirmation,
 }: RegisterProps) => {
-  // If the values are null, return it.
+  // If the values are null, return false.
   if (!name || !email || !password || !passConfirmation) {
     alert("Please fill in all fields"); // Set alert message temporarily.
     return false;
   }
 
-  // TODO: Check if the number of characters in the name is less than XX, return it.
+  // TODO: Check if the number of characters in the name is less than XX, return false.
 
-  // If the email is not in the correct format, return it.
+  // If the email is not in the correct format, return false.
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (!emailPattern.test(email)) {
     alert("Please enter a valid email address"); // Set alert message temporarily.
     return false;
   }
 
-  // TODO: Check if the number of characters in the password is less than XX, return it.
+  // TODO: Check if the number of characters in the password is less than XX, return false.
 
-  // If values of password and password confirmation is different, return it.
+  // If values of password and password confirmation is different, return false.
   if (password !== passConfirmation) {
     alert("Password and password confirmation do not match"); // Set alert message temporarily.
     return false;
