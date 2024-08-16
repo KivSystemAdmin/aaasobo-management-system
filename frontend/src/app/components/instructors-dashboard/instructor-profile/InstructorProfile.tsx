5e141dc2630a4e09c0a7a6870bf98351a570b6b9
Saeka03 <143541044+Saeka03@users.noreply.github.com>
2024-08-16 10:04:40 -0700

    Feature/226 style the instructor profile page (#227)
    * Style the instructor profile page
    
    * Add favicon

--- frontend/src/app/components/instructors-dashboard/instructor-profile/InstructorProfile.tsx
+++ frontend/src/app/components/instructors-dashboard/instructor-profile/InstructorProfile.tsx
@@ -4,6 +4,13 @@ import { getInstructor } from "@/app/helper/instructorsApi";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./InstructorProfile.module.scss";
+import {
+  EnvelopeIcon,
+  InformationCircleIcon,
+  LinkIcon,
+  UserCircleIcon,
+  VideoCameraIcon,
+} from "@heroicons/react/24/outline";
 
 function InstructorProfile({
   instructorId,
@@ -33,50 +40,85 @@ function InstructorProfile({
 
   return (
     <>
-      <h1>Profile Page</h1>
-      {instructor ? (
-        <>
-          <Image
-            src={`/instructors/${instructor.icon}`}
-            alt={instructor.name}
-            width={100}
-            height={100}
-            priority
-          />
-          <h3>Name</h3> <p>{instructor.name}</p>
-          <h3>Nickname</h3> <p>{instructor.nickname}</p>
-          <h3>Email</h3> <p>{instructor.email}</p>
-          <h3>Class URL</h3>{" "}
-          <p>
-            <a
-              href={instructor.classURL}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {instructor.classURL}
-            </a>
-          </p>
-          <h3>Meeting ID</h3> <p>{instructor.meetingId}</p>
-          <h3>Passcode</h3> <p>{instructor.passcode}</p>
-          <h3>Self Introduction URL</h3>{" "}
-          <p>
-            <a
-              href={instructor.introductionURL}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {instructor.introductionURL}
-            </a>
-          </p>
-          <br />
-          <p>
-            ■ If you wish to update the profile information above, please
-            contact the staff via Facebook.
-          </p>
-        </>
-      ) : (
-        <div className={styles.loadingContainer}>Loading ...</div>
-      )}
+      <h2>Profile Page</h2>
+      <div className={styles.container}>
+        {instructor ? (
+          <>
+            <Image
+              src={`/instructors/${instructor.icon}`}
+              alt={instructor.name}
+              width={100}
+              height={100}
+              priority
+              className={styles.pic}
+            />
+            <p>Name</p> <h3>{instructor.name}</h3>
+            <div className={styles.insideContainer}>
+              <UserCircleIcon className={styles.icon} />
+              <div>
+                <p>Nickname</p>
+                <h4>{instructor.nickname}</h4>
+              </div>
+            </div>
+            <div className={styles.insideContainer}>
+              <EnvelopeIcon className={styles.icon} />
+              <div>
+                <p>Email</p>
+                <h4>{instructor.email}</h4>
+              </div>
+            </div>
+            <div className={styles.insideContainer}>
+              <VideoCameraIcon className={styles.icon} />
+              <div>
+                <p>Class URL</p>
+                <h4>
+                  <a
+                    href={instructor.classURL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.url}
+                  >
+                    {instructor.classURL}
+                  </a>
+                </h4>
+                <div className={styles.urlInfo}>
+                  <p>Meeting ID: </p>
+                  <p>{instructor.meetingId}</p>
+                </div>
+                <div className={styles.urlInfo}>
+                  <p>Passcode: </p>
+                  <p>{instructor.passcode}</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.insideContainer}>
+              <LinkIcon className={styles.icon} />
+              <div>
+                <p>Self Introduction URL</p>
+                <h4>
+                  <a
+                    href={instructor.introductionURL}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.url}
+                  >
+                    {instructor.introductionURL}
+                  </a>
+                </h4>
+              </div>
+            </div>
+            <div className={styles.insideContainer}>
+              <InformationCircleIcon className={styles.icon} />
+              <p className={styles.info}>
+                If you wish to update the profile information above, please
+                contact the staff via Facebook.
+              </p>
+            </div>
+          </>
+        ) : (
+          <div className={styles.loadingContainer}>Loading ...</div>
+        )}
+      </div>
     </>
   );
 }
