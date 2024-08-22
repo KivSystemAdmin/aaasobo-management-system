204911b2bf946029af62cf80020f853340f4fc9e
Shingo023 <144206499+Shingo023@users.noreply.github.com>
2024-08-21 20:16:17 -0700

    Implement and styled the Customer Calendar without using a custom Cal… (#279)
    * Implement and styled the Customer Calendar without using a custom CalendarHeader.
    
    * Center the calendar header title.

--- frontend/src/app/components/CalendarView.tsx
+++ frontend/src/app/components/CalendarView.tsx
@@ -1,10 +1,4 @@
-import React, {
-  useRef,
-  forwardRef,
-  useImperativeHandle,
-  useState,
-  useEffect,
-} from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import FullCalendar from "@fullcalendar/react";
@@ -14,7 +8,6 @@ import interactionPlugin from "@fullcalendar/interaction";
 import momentTimezonePlugin from "@fullcalendar/moment-timezone";
 import styles from "./CalendarHeaderView.module.scss";
 import {
-  CalendarApi,
   DayCellContentArg,
   EventClickArg,
   EventContentArg,
@@ -53,282 +46,259 @@ type InstructorCalendarViewProps = {
   fetchData?: () => void;
 };
 
-type CalendarViewRefType = {
-  getApi: () => CalendarApi;
-};
+const CalendarView: React.FC<InstructorCalendarViewProps> = ({
+  events,
+  holidays,
+  customerId,
+  instructorId,
+  isAdminAuthenticated,
+  fetchData,
+}) => {
+  const [isClassDetailModalOpen, setIsClassDetailModalOpen] = useState(false);
+  const [classes, setClasses] = useState<ClassType[] | null>(null);
+  const [classDetail, setClassDetail] = useState<ClassType | null>(null);
+  const router = useRouter();
 
-// Defines the CalendarView component using forwardRef to pass a ref down to FullCalendar
-const CalendarView = forwardRef<
-  CalendarViewRefType,
-  InstructorCalendarViewProps
->(
-  (
-    {
-      events,
-      holidays,
-      customerId,
-      instructorId,
-      isAdminAuthenticated,
-      fetchData,
-    },
-    ref,
-  ) => {
-    const [isClassDetailModalOpen, setIsClassDetailModalOpen] = useState(false);
-    const [classes, setClasses] = useState<ClassType[] | null>(null);
-    // const [selectedCLassId, setSelectedClassId] = useState<number | null>(null);
-    const [classDetail, setClassDetail] = useState<ClassType | null>(null);
-    const calendarRef = useRef<FullCalendar | null>(null);
-    const router = useRouter();
+  const fetchClasses = async () => {
+    if (!customerId) return;
 
-    const fetchClasses = async () => {
-      if (!customerId) return;
-
-      try {
-        const classes: ClassType[] = await getClassesByCustomerId(
-          customerId.toString(),
-        );
-        setClasses(classes);
-      } catch (error) {
-        console.error("Failed to fetch classes:", error);
-      }
-    };
-
-    useEffect(() => {
-      fetchClasses();
-    }, [customerId, classes]);
-
-    // Uses useImperativeHandle to define the getApi method and expose it to the parent Page component
-    useImperativeHandle(ref, () => ({
-      getApi: () => {
-        const api = calendarRef.current?.getApi();
-        if (!api) {
-          throw new Error("Calendar API is not available.");
-        }
-        return api;
-      },
-    }));
-
-    // Formats and displays the content of an event on the calendar view page
-    const renderEventContent = (eventInfo: EventContentArg) => {
-      const startDate = new Date(
-        new Date(eventInfo.event.startStr).toLocaleString("en-US", {
-          timeZone: instructorId ? "Asia/Manila" : "Asia/Tokyo",
-        }),
+    try {
+      const classes: ClassType[] = await getClassesByCustomerId(
+        customerId.toString(),
       );
-      const hours = startDate.getHours();
-      const minutes = String(startDate.getMinutes()).padStart(2, "0");
-      const formattedStartTime = `${hours}:${minutes}`;
+      setClasses(classes);
+    } catch (error) {
+      console.error("Failed to fetch classes:", error);
+    }
+  };
 
-      const { instructorIcon, instructorNickname, classStatus } =
-        eventInfo.event.extendedProps;
-      const { title } = eventInfo.event;
+  useEffect(() => {
+    fetchClasses();
+  }, [customerId, classes]);
 
-      const isClickable = eventInfo.event.title !== "No booked class";
+  // Formats and displays the content of an event on the calendar view page
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    const startDate = new Date(
+      new Date(eventInfo.event.startStr).toLocaleString("en-US", {
+        timeZone: instructorId ? "Asia/Manila" : "Asia/Tokyo",
+      }),
+    );
+    const hours = startDate.getHours();
+    const minutes = String(startDate.getMinutes()).padStart(2, "0");
+    const formattedStartTime = `${hours}:${minutes}`;
 
-      return (
-        <div
-          className={styles.eventBlock}
-          style={{
-            cursor: isClickable ? "pointer" : "default",
-          }}
-        >
-          {classStatus === "booked" && instructorIcon ? (
-            <Image
-              src={`/instructors/${instructorIcon}`}
-              alt={instructorNickname || "Instructor"}
-              width={30}
-              height={30}
-              priority
-              className={styles.instructorIcon}
-            />
-          ) : classStatus === "completed" ? (
-            <div className={styles.classStatusIcon}>
-              <CheckCircleIcon className={styles.classStatusIcon__completed} />
-            </div>
-          ) : classStatus === "canceledByCustomer" ? (
-            <div className={styles.classStatusIcon}>
-              <XCircleIcon className={styles.classStatusIcon__canceled} />
-            </div>
-          ) : classStatus === "canceledByInstructor" ? (
-            <div className={styles.classStatusIcon}>
-              <ExclamationTriangleIcon
-                className={styles.classStatusIcon__canceled}
-              />
-            </div>
-          ) : null}
-          <div
-            className={`${styles.eventDetails} ${classStatus === "booked" ? styles.booked : classStatus === "completed" ? styles.completed : classStatus === "canceledByCustomer" || classStatus === "canceledByInstructor" ? styles.canceled : ""}`}
-          >
-            <div className={styles.eventTime}>{formattedStartTime} -</div>
-            <div className={styles.eventTitle}>{title}</div>
-          </div>
-        </div>
-      );
-    };
+    const { instructorIcon, instructorNickname, classStatus } =
+      eventInfo.event.extendedProps;
+    const { title } = eventInfo.event;
 
-    // // TODO: Applies custom styles to calendar cells based on holiday dates
-    // const dayCellDidMount = (info: DayCellContentArg) => {
-    //   const date = new Date(info.date);
-    //   const formattedDate = date.toISOString().split("T")[0];
-
-    //   if (!holidays.includes(formattedDate)) {
-    //     return;
-    //   }
-    //   info.el.classList.add(styles.holidayCell);
-    //   const dayNumber = info.el.querySelector(".fc-daygrid-day-number");
-    //   if (dayNumber) {
-    //     dayNumber.classList.add(styles.holidayDateNumber);
-    //   }
-    // };
-
-    const handleEventClick = (clickInfo: EventClickArg) => {
-      const classId = clickInfo.event.extendedProps.classId;
-      if (instructorId && clickInfo.event.title !== "No booked class") {
-        if (isAdminAuthenticated) {
-          router.push(
-            `/admins/instructor-list/${instructorId}/class-schedule/${classId}`,
-          );
-          return;
-        }
-        router.push(`/instructors/${instructorId}/class-schedule/${classId}`);
-      } else if (customerId) {
-        if (!classes) return;
-        const selectedClassDetail = classes.find(
-          (eachClass) => eachClass.id === classId,
-        );
-        selectedClassDetail && setClassDetail(selectedClassDetail);
-        setIsClassDetailModalOpen(true);
-        // If the Afmin dashboard uses class details page, instead of the modal, the code below should be used
-        // if (isAdminAuthenticated) {
-        //   router.push(`/admins/customer-list/${customerId}/classes/${classId}`);
-        //   return;
-        // }
-        // router.push(`/customers/${customerId}/classes/${classId}`);
-      }
-    };
-
-    // To resoleve the VS code complaint
-    CalendarView.displayName = "CalendarView";
-
-    const validRange = () => {
-      const now = new Date();
-      // TODO: 'start' should be the Instructor's 'createdDate'
-      const start = new Date(2024, 0, 1);
-      // The calendar can be viewed until the end of the month, two months ahead.
-      const end = new Date(now.getFullYear(), now.getMonth() + 3, 0);
-      return {
-        start: start.toISOString().split("T")[0],
-        end: end.toISOString().split("T")[0],
-      };
-    };
-
-    const handleModalClose = () => {
-      setClassDetail(null);
-      setIsClassDetailModalOpen(false);
-    };
-
-    const handleCancel = async (classId: number, classDateTime: string) => {
-      const isPastPreviousDay = isPastPreviousDayDeadline(
-        classDateTime,
-        "Asia/Tokyo",
-      );
-
-      if (isPastPreviousDay)
-        return alert(
-          "Classes cannot be canceled on or after the scheduled day of the class.",
-        );
-
-      const confirmed = window.confirm(
-        "Are you sure you want to cancel this class?",
-      );
-      if (!confirmed) return;
-      try {
-        await cancelClass(classId);
-        fetchData?.();
-        fetchClasses();
-        handleModalClose();
-        toast.success("The class has been successfully canceled!");
-      } catch (error) {
-        console.error("Failed to cancel the class:", error);
-      }
-    };
+    const isClickable = eventInfo.event.title !== "No booked class";
 
     return (
-      <>
-        <FullCalendar
-          ref={calendarRef}
-          plugins={[
-            dayGridPlugin,
-            timeGridPlugin,
-            interactionPlugin,
-            momentTimezonePlugin,
-          ]}
-          // initialView={instructorId ? "timeGridWeek" : "dayGridMonth"}
-          initialView={"dayGridMonth"}
-          headerToolbar={
-            instructorId
-              ? {
-                  left: "prev,next today",
-                  center: "title",
-                  right: "dayGridMonth,timeGridWeek,timeGridDay",
-                }
-              : false
-          }
-          views={{
-            timeGridWeek: {
-              slotMinTime: "08:00:00",
-              slotMaxTime: "20:30:00",
-              slotLabelFormat: {
-                hour: "2-digit",
-                minute: "2-digit",
-                hour12: false,
-              },
-            },
-            timeGridDay: {
-              slotMinTime: "08:00:00",
-              slotMaxTime: "20:30:00",
-              slotLabelFormat: {
-                hour: "2-digit",
-                minute: "2-digit",
-                hour12: false,
-              },
-            },
-          }}
-          events={events}
-          eventClick={handleEventClick}
-          eventContent={renderEventContent}
-          validRange={validRange}
-          // TODO: After the 'Holiday' table is created, apply the styling to them
-          // dayCellDidMount={dayCellDidMount}
-          locale="en"
-          contentHeight="auto"
-          dayMaxEvents={true}
-          editable={false}
-          selectable={false}
-          eventDisplay="block"
-          allDaySlot={false}
-          timeZone={instructorId ? "Asia/Manila" : "Asia/Tokyo"}
-        />
-
-        <Modal isOpen={isClassDetailModalOpen} onClose={handleModalClose}>
-          <div className={styles.modal}>
-            {customerId ? (
-              <ClassDetail
-                classDetail={classDetail}
-                customerId={customerId.toString()}
-                timeZone="Asia/Tokyo"
-                handleCancel={handleCancel}
-                isAdminAuthenticated
-                handleModalClose={handleModalClose}
-              />
-            ) : (
-              <p>No customer ID available</p>
-            )}
+      <div
+        className={styles.eventBlock}
+        style={{
+          cursor: isClickable ? "pointer" : "default",
+        }}
+      >
+        {classStatus === "booked" && instructorIcon ? (
+          <Image
+            src={`/instructors/${instructorIcon}`}
+            alt={instructorNickname || "Instructor"}
+            width={30}
+            height={30}
+            priority
+            className={styles.instructorIcon}
+          />
+        ) : classStatus === "completed" ? (
+          <div className={styles.classStatusIcon}>
+            <CheckCircleIcon className={styles.classStatusIcon__completed} />
           </div>
-        </Modal>
-      </>
+        ) : classStatus === "canceledByCustomer" ? (
+          <div className={styles.classStatusIcon}>
+            <XCircleIcon className={styles.classStatusIcon__canceled} />
+          </div>
+        ) : classStatus === "canceledByInstructor" ? (
+          <div className={styles.classStatusIcon}>
+            <ExclamationTriangleIcon
+              className={styles.classStatusIcon__canceled}
+            />
+          </div>
+        ) : null}
+        <div
+          className={`${styles.eventDetails} ${classStatus === "booked" ? styles.booked : classStatus === "completed" ? styles.completed : classStatus === "canceledByCustomer" || classStatus === "canceledByInstructor" ? styles.canceled : ""}`}
+        >
+          <div className={styles.eventTime}>{formattedStartTime} -</div>
+          <div className={styles.eventTitle}>{title}</div>
+        </div>
+      </div>
     );
-  },
-);
+  };
+
+  // // TODO: Applies custom styles to calendar cells based on holiday dates
+  // const dayCellDidMount = (info: DayCellContentArg) => {
+  //   const date = new Date(info.date);
+  //   const formattedDate = date.toISOString().split("T")[0];
+
+  //   if (!holidays.includes(formattedDate)) {
+  //     return;
+  //   }
+  //   info.el.classList.add(styles.holidayCell);
+  //   const dayNumber = info.el.querySelector(".fc-daygrid-day-number");
+  //   if (dayNumber) {
+  //     dayNumber.classList.add(styles.holidayDateNumber);
+  //   }
+  // };
+
+  const handleEventClick = (clickInfo: EventClickArg) => {
+    const classId = clickInfo.event.extendedProps.classId;
+    if (instructorId && clickInfo.event.title !== "No booked class") {
+      if (isAdminAuthenticated) {
+        router.push(
+          `/admins/instructor-list/${instructorId}/class-schedule/${classId}`,
+        );
+        return;
+      }
+      router.push(`/instructors/${instructorId}/class-schedule/${classId}`);
+    } else if (customerId) {
+      if (!classes) return;
+      const selectedClassDetail = classes.find(
+        (eachClass) => eachClass.id === classId,
+      );
+      selectedClassDetail && setClassDetail(selectedClassDetail);
+      setIsClassDetailModalOpen(true);
+      // If the Afmin dashboard uses class details page, instead of the modal, the code below should be used
+      // if (isAdminAuthenticated) {
+      //   router.push(`/admins/customer-list/${customerId}/classes/${classId}`);
+      //   return;
+      // }
+      // router.push(`/customers/${customerId}/classes/${classId}`);
+    }
+  };
+
+  // To resoleve the VS code complaint
+  CalendarView.displayName = "CalendarView";
+
+  const validRange = () => {
+    const now = new Date();
+    // TODO: 'start' should be the Instructor's 'createdDate'
+    const start = new Date(2024, 0, 1);
+    // The calendar can be viewed until the end of the month, two months ahead.
+    const end = new Date(now.getFullYear(), now.getMonth() + 3, 0);
+    return {
+      start: start.toISOString().split("T")[0],
+      end: end.toISOString().split("T")[0],
+    };
+  };
+
+  const handleModalClose = () => {
+    setClassDetail(null);
+    setIsClassDetailModalOpen(false);
+  };
+
+  const handleCancel = async (classId: number, classDateTime: string) => {
+    const isPastPreviousDay = isPastPreviousDayDeadline(
+      classDateTime,
+      "Asia/Tokyo",
+    );
+
+    if (isPastPreviousDay)
+      return alert(
+        "Classes cannot be canceled on or after the scheduled day of the class.",
+      );
+
+    const confirmed = window.confirm(
+      "Are you sure you want to cancel this class?",
+    );
+    if (!confirmed) return;
+    try {
+      await cancelClass(classId);
+      fetchData?.();
+      fetchClasses();
+      handleModalClose();
+      toast.success("The class has been successfully canceled!");
+    } catch (error) {
+      console.error("Failed to cancel the class:", error);
+    }
+  };
+
+  return (
+    <>
+      <FullCalendar
+        plugins={[
+          dayGridPlugin,
+          timeGridPlugin,
+          interactionPlugin,
+          momentTimezonePlugin,
+        ]}
+        // initialView={instructorId ? "timeGridWeek" : "dayGridMonth"}
+        initialView={"dayGridMonth"}
+        headerToolbar={
+          instructorId
+            ? {
+                left: "prev,next today",
+                center: "title",
+                right: "dayGridMonth,timeGridWeek,timeGridDay",
+              }
+            : {
+                left: "prev,next today",
+                center: "title",
+                right: "",
+              }
+        }
+        views={{
+          timeGridWeek: {
+            slotMinTime: "08:00:00",
+            slotMaxTime: "20:30:00",
+            slotLabelFormat: {
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: false,
+            },
+          },
+          timeGridDay: {
+            slotMinTime: "08:00:00",
+            slotMaxTime: "20:30:00",
+            slotLabelFormat: {
+              hour: "2-digit",
+              minute: "2-digit",
+              hour12: false,
+            },
+          },
+        }}
+        events={events}
+        eventClick={handleEventClick}
+        eventContent={renderEventContent}
+        validRange={validRange}
+        // TODO: After the 'Holiday' table is created, apply the styling to them
+        // dayCellDidMount={dayCellDidMount}
+        locale="en"
+        contentHeight="auto"
+        dayMaxEvents={true}
+        editable={false}
+        selectable={false}
+        eventDisplay="block"
+        allDaySlot={false}
+        timeZone={instructorId ? "Asia/Manila" : "Asia/Tokyo"}
+      />
+
+      <Modal isOpen={isClassDetailModalOpen} onClose={handleModalClose}>
+        <div className={styles.modal}>
+          {customerId ? (
+            <ClassDetail
+              classDetail={classDetail}
+              customerId={customerId.toString()}
+              timeZone="Asia/Tokyo"
+              handleCancel={handleCancel}
+              isAdminAuthenticated
+              handleModalClose={handleModalClose}
+            />
+          ) : (
+            <p>No customer ID available</p>
+          )}
+        </div>
+      </Modal>
+    </>
+  );
+};
 
 export default CalendarView;
--- frontend/src/app/components/customers-dashboard/classes/ClassCalendar.tsx
+++ frontend/src/app/components/customers-dashboard/classes/ClassCalendar.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
-import CalendarHeader from "@/app/components/CalendarHeader";
 import CalendarView from "@/app/components/CalendarView";
 import styles from "./ClassCalendar.module.scss";
 import FullCalendar from "@fullcalendar/react";
-import { CalendarApi } from "@fullcalendar/core/index.js";
 import {
   formatFiveMonthsLaterEndOfMonth,
   isPastPreviousDayDeadline,
@@ -26,7 +24,6 @@ function ClassCalendar({
 }) {
   const [classesForCalendar, setClassesForCalendar] = useState<EventType[]>([]);
   const [classes, setClasses] = useState<ClassForCalendar[] | null>(null);
-  const [calendarApi, setCalendarApi] = useState<CalendarApi | null>(null);
   const [rebookableClasses, setRebookableClasses] = useState<
     ClassForCalendar[] | undefined
   >(undefined);
@@ -38,8 +35,6 @@ function ClassCalendar({
   const [isCancelingModalOpen, setIsCancelingModalOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
-
-  const calendarRef = useRef<FullCalendar | null>(null);
 
   const fetchData = async () => {
     try {
@@ -117,13 +112,6 @@ function ClassCalendar({
     fetchData();
   }, [customerId]);
 
-  useEffect(() => {
-    // Only set calendarApi if calendarRef is not null
-    if (calendarRef.current) {
-      setCalendarApi(calendarRef.current.getApi());
-    }
-  }, [calendarRef.current]);
-
   const handleCancelingModalClose = () => {
     setIsCancelingModalOpen(false);
     setSelectedClasses([]);
@@ -191,17 +179,12 @@ function ClassCalendar({
   if (error) return <div>{error}</div>;
 
   return (
-    // 'Page' is the parent component of 'CalendarHeader' and 'CalendarView' children components.
-    // 'CalendarHeader' and 'CalendarView' are initially independent from each other, but 'Page' can connect them together
-    // by passing 'calendarRef' to 'CalendarView' and retrieving the FullCalendar API instance from it and making the API available for 'CalendarHeader'using state
     <div className={styles.calendarContainer}>
       {isLoading ? (
         <div className={styles.loadingContainer}>Loading ...</div>
       ) : (
         <>
           <div className={styles.calendarHeaderContainer}>
-            <CalendarHeader calendarApi={calendarApi ?? null} />
-
             <div className={styles.calendarActions}>
               <div className={styles.calendarActions__container}>
                 <div className={styles.calendarActions__canceling}>
@@ -247,8 +230,6 @@ function ClassCalendar({
           </div>
 
           <CalendarView
-            // Create a ref to access the FullCalendar instance in CalendarView;
-            ref={calendarRef}
             events={classesForCalendar}
             // TODO: Fetch holidays from the backend
             // holidays={["2024-07-29", "2024-07-30", "2024-07-31"]}
