8f29c3c1023a5d92651a6752226f74161a1d5fc6
Masato Takakusaki <143541083+mtkksk1780@users.noreply.github.com>
2024-09-21 01:49:56 +0700

    Update next.config.mjs (#23)
    * Update next.config.mjs
    
    * Update next.config.mjs
    
    * Update next.config.mjs
    
    ---------
    
    Co-authored-by: KivSystemAdmin <kiv.developers@gmail.com>

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://aaasobo-managament-system-frontend.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
