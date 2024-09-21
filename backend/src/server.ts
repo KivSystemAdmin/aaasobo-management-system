133201e8963db5e7ed1474e2cbdf62ba1cd1a190
Masato Takakusaki <143541083+mtkksk1780@users.noreply.github.com>
2024-09-21 17:19:04 +0700

    Update files (#41)
    Co-authored-by: KivSystemAdmin <kiv.developers@gmail.com>

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -24,7 +24,7 @@ const KEY2 = process.env.KEY2 || "";
 
 // Middleware
 server.use(express.json()); // to parse JSON bodies
-server.use("*", cors(corsOptions)); // CORS settings for all routes
+server.use(cors(corsOptions)); // CORS settings for all routes
 // Cookie-session
 server.use(
   cookieSession({
