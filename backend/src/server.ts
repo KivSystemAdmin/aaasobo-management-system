308f008a52b2fa7802eb83692a87f5d5a18ad569
Masato Takakusaki <143541083+mtkksk1780@users.noreply.github.com>
2024-09-22 22:36:59 +0700

    80 fix cors issue (#81)
    * Update file
    
    * Update package.json
    
    * Update package.json
    
    ---------
    
    Co-authored-by: KivSystemAdmin <kiv.developers@gmail.com>

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ export const server = express();
 
 // List of allowed origins
 const allowedOrigins = [
-  "https://aaasobo-management-system-frontend.vercel.app",
+  "https://aaasobo-managament-system-frontend.vercel.app",
   "http://localhost:3000",
 ];
 
@@ -27,7 +27,7 @@ server.use(
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error("CORS policy violation"));
+        callback(new Error("CORS policy violation!"));
       }
     },
     credentials: true,
