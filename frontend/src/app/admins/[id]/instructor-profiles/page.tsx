7a7e7ba805125a5acf3167fb66006c3759529516
mtkksk1780 <mtkksk1780@gmail.com>
2026-03-24 22:18:19 +0700

    feat: add instructor profiles page to admin dashboard

---
+++ frontend/src/app/admins/[id]/instructor-profiles/page.tsx
@@ -0,0 +1,42 @@
+import InstructorsList from "@/components/customers-dashboard/instructor-profiles/InstructorsList";
+import { getAllInstructorProfiles } from "@/lib/api/instructorsApi";
+import { authenticateUserSession } from "@/lib/auth/sessionUtils";
+import { getCookie } from "../../../../proxy";
+
+async function InstructorProfilesPage({
+  params,
+  searchParams,
+}: {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ instructorId?: string; customerId?: string }>;
+}) {
+  const { id: adminId } = await params;
+  const { instructorId, customerId } = await searchParams;
+  // Authenticate user session
+  const userSessionType: UserType = await authenticateUserSession(
+    "admin",
+    adminId,
+  );
+
+  // Get the cookies from the request headers
+  const cookie = await getCookie();
+
+  // Fetch instructor profiles
+  const instructorProfiles = await getAllInstructorProfiles(cookie);
+
+  // Show error message when no instructor profiles are found
+  if (!instructorProfiles || instructorProfiles.length === 0) {
+    return <p>Error: No instructor profiles found.</p>;
+  }
+
+  return (
+    <>
+      <InstructorsList
+        instructorProfiles={instructorProfiles}
+        userSessionType={userSessionType}
+      />
+    </>
+  );
+}
+
+export default InstructorProfilesPage;
