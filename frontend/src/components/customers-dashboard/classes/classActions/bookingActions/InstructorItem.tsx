6a5e21f84d7d92075d85ea8511a51d0c8e178f47
mtkksk1780 <mtkksk1780@gmail.com>
2026-03-26 01:15:16 +0700

    refactor: unify instructor image display processes

--- frontend/src/components/customers-dashboard/classes/classActions/bookingActions/InstructorItem.tsx
+++ frontend/src/components/customers-dashboard/classes/classActions/bookingActions/InstructorItem.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import styles from "./InstructorItem.module.scss";
 import { textLight } from "@/styles/colors";
+import { defaultUserImageUrl } from "@/lib/data/data";
 
 interface InstructorItemProps {
   instructor: InstructorRebookingProfile;
@@ -56,30 +57,14 @@ export default function InstructorItem({
       <div className={styles.instructorItem__actions}>
         <div className={styles.instructorItem__instructor}>
           <div className={styles.instructorPhoto}>
-            {imageError ? (
-              <div
-                className={
-                  styles.placeholderImage + " " + styles[englishBackgroundClass]
-                }
-              >
-                <svg
-                  width="50"
-                  height="50"
-                  viewBox="0 0 24 24"
-                  fill={textLight}
-                >
-                  <path d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
-                </svg>
-              </div>
-            ) : (
-              <Image
-                src={`/instructors/${instructor.icon}`}
-                alt={instructor.nickname}
-                width={50}
-                height={50}
-                onError={handleImageError}
-              />
-            )}
+            <Image
+              src={imageError ? defaultUserImageUrl : `${instructor.icon}`}
+              alt={instructor.nickname}
+              width={50}
+              height={50}
+              unoptimized
+              onError={handleImageError}
+            />
           </div>
           <div onClick={handleProfileClick} className={styles.instructorName}>
             <a
